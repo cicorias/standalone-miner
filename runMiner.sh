7b4cb88e4b82f996a85a603684a38cd3b6dbc170
Shawn Cicoria <github@cicoria.com>
2017-06-02 23:24:20 -0400

    move ethash

--- runMiner.sh
+++ runMiner.sh
@@ -4,4 +4,10 @@ set -x
 
 ADDRESS=$(<address.txt)
 
-./geth --datadir ./data --networkid 123 --nodiscover --maxpeers 0 --mine --minerthreads 1 --etherbase $ADDRESS --rpc --rpcapi "admin,debug,eth,miner,net,personal,rpc,txpool,web3"
+
+./geth --datadir ./data --networkid 123 --nodiscover --maxpeers 0 \
+    --nat none \
+    --ethash.dagdir $(pwd)/.ethash \
+    --mine --minerthreads 1 --etherbase $ADDRESS \
+    --rpc \
+    --rpcapi "admin,debug,eth,miner,net,personal,rpc,txpool,web3"
